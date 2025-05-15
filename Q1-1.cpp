bb343b84f66e07028af163db68c320fceeb5d218
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-05-15 14:54:34 +0700

    1-1/1-2

--- Q1-1.cpp
+++ Q1-1.cpp
@@ -5,24 +5,10 @@ int** identity(int n) {
     int** m = new int*[n];
     for (int i = 0; i < n; i++) {
         m[i] = new int[n];
-        for (int j = 0; j < n; j++ ) {
-            m[i][j] = ( i == j) ? 1:0;
+        for (int j = 0; j < n; j++) {
+            m[i][j] = (i == j) ? 1 : 0;
         }
     }
     return m;
 }
 
-int main() {
-    int n;
-    cout <<"ENter the size of the identity matrix: ";
-    cin >> n;
-    int** m = identity(n);
-    for (int i = 0; i < n; i++) {
-        for (int j=0; j<n; j++) {
-            cout << m[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
-}
--- Q1-2.cpp
+++ Q1-2.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int* singleBlock(int n) {
     int* a = new int[n];
     for (int i = 0; i <= n; i++) {
-        a[i] = new int[n]
+        a[i] = new int[n];
         for (int j = i; j <= n; j++)
         {
-            a[i][j] = (i == j) 1
+            a[i][j] = (i == j) 1;
         }
         cout << endl;
     }
