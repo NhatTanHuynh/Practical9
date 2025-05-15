e28157f054410e0c9a0f3c337725ed314361db00
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-05-15 14:32:30 +0700

    q1-1

---
+++ Q1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int** identity(int n) {
+    int* a = new int[n];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        m[i] = new int[n];
+        for (int j = 0; j < n; j++ ) {
+            m[i][j] = ( i == j) ? 1:0;
+        }
+    }
+    return m;
+}
