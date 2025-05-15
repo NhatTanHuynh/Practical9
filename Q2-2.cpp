d208d3f9303adfda6cc6e7fca381e94784b1aacf
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-05-15 16:19:59 +0700

    fix

--- Q2-2.cpp
+++ Q2-2.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int* singleBlock(int n) {
-    int* a = new int[n * n];
-    for (int i=0; i < n; i++) {
-        for (int j = 0; j < n; j++){
-            a[i * n + j] = i;
-        }
-    }
-    return a;
-}
-
 void setColumn(int* matrix, int n, int c, int newValue) {
     for (int i =0; i < n;i++) {
         matrix[i * n + c] = newValue;
